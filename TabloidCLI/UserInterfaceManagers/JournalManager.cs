6cec3e2ee7fabbe7e9377f9b16f9fb0b98e8941f
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-12-08 08:37:55 -0600

    can delete entries and return to main menu

--- TabloidCLI/UserInterfaceManagers/JournalManager.cs
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -23,7 +23,7 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.WriteLine(" 1) List Journal Entries");
             Console.WriteLine(" 2) Add New Journal Entry");
             //Console.WriteLine(" 3) Edit Journal Entry");
-            //Console.WriteLine(" 4) Remove Journal Entry");
+            Console.WriteLine(" 4) Remove Journal Entry");
             Console.WriteLine(" 0) Return to Main Menu");
             //Console.Write("> ");
             string choice = Console.ReadLine();
@@ -35,6 +35,11 @@ namespace TabloidCLI.UserInterfaceManagers
                 case "2":
                     Add();
                     return this;
+                case "4":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
                 default:
                     Console.WriteLine("Invalid Selection");
                     return this;
@@ -60,15 +65,58 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.WriteLine("New Journal");
             Journal journal = new Journal();
 
-            Console.Write("title ");
+            Console.Write("Entry Title: ");
             journal.Title = Console.ReadLine();
 
             journal.CreateDateTime = DateTime.Now;
 
-            Console.Write("Entry ");
+            Console.Write("What's on your mind?");
             journal.Content = Console.ReadLine();
 
             _journalRepository.Insert(journal);
+        }
+     //Delete an entry
+        private void Remove()
+        {
+            Journal selectedEntry = Choose("What journal entry do you want to delete?");
+            if (selectedEntry != null)
+            {
+               _journalRepository.Delete(selectedEntry.Id);
+            }
+        }
+     //Select one
+        private Journal Choose(string prompt = null)
+    //Ask for selection
+        {
+            if (prompt == null)
+            {
+                prompt = "Select an entry";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Journal> entries = _journalRepository.GetAll();
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                Journal journal = entries[i];
+                Console.WriteLine($" {i + 1}) {journal.Title}");
+            }
+            Console.Write("> ");
+    //Read the input of selected entry
+
+            string input = Console.ReadLine();
+ 
+            try
+            {
+                int choice = int.Parse(input);
+                return entries[choice - 1];
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
         }
     }
 }
